4b3db5fcfe8a51c4a54483f1b2658581e2c96426
leloucj <gothicj@163.com>
2017-05-21 23:23:23 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,42 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(inputs) {
+  var nums = inputs
+  for(var i=0;i<nums.length;i++){
+      var num=parseInt(nums[i])
+      switch(num)
+      {
+          case 0:
+              var lcd="._.
+|.|
+|_|"
+              return lcd
+              break;
+          case 1:
+              var lcd ="...
+..|
+..|"
+              return lcd
+              break;
+          case 2:
+              var lcd ="._.
+._|
+|_."
+              return lcd
+              break;
+          case 3:
+              var lcd ="._.
+._|
+._|"
+              return lcd
+              break;
+              
+          case 9:
+               var lcd ="._.
+|_|
+..|"
+              return lcd
+              break;
+      }
+  }
+    
+    
 };
