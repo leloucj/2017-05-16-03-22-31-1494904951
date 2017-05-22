c9ef7edf6514b1e61ac3248f10ee722def5d138c
leloucj <gothicj@163.com>
2017-05-22 09:40:18 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -6,8 +6,8 @@ module.exports = function main(inputs) {
     var tArray = new Array();  //先声明一维
     for(var k=0;k<3;k++){    //一维长度为i,i为变量，可以根据实际情况改
        tArray[k]=new Array();  //声明二维，每一个一维数组里面的一个元素都是一个数组；
-       for(var j=0;j<4*num.length;j++){   //一维数组里面每个元素数组可以包含的数量p，p也是一个变量；
-       tArray[k][j]=" ";    //这里将变量初始化，我这边统一初始化为空，后面在用所需的值覆盖里面的值
+       for(var j=0;j<3*num.length;j++){   //一维数组里面每个元素数组可以包含的数量p，p也是一个变量；
+       tArray[k][j]="";    //这里将变量初始化，我这边统一初始化为空，后面在用所需的值覆盖里面的值
          }
     }
     for(var i=0;i<num.length;i++)
@@ -95,9 +95,9 @@ module.exports = function main(inputs) {
      }
     
    var print=new Array();
-    print[0]=tArray[0].join("");
-    print[1]=tArray[1].join("");
-    print[2]=tArray[2].join("");
+    print[0]=tArray[0].join(" ");
+    print[1]=tArray[1].join(" ");
+    print[2]=tArray[2].join(" ");
     var list=print[0]+"\n"+"         "+print[1]+"\n"+"         "+print[2]+"\n";
 return list;
 };
